1b5a3fba9b944d658cbc7d4965194839c17604b4
navistar792 <r.verner99@yahoo.com>
2021-10-26 15:24:28 -0500

    display data using flask

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
 
 import psycopg2
 from config import config
+from flask import Flask, jsonify, request, render_template
 
-def get_parts():
-    """ query parts from the parts table """
-    conn = None
-    try:
-        params = config()
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM category2")
-        rows = cur.fetchall()
-        print("The number of parts: ", cur.rowcount)
-        for row in rows:
-            print(row)
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+# def get_parts():
+    # """ query parts from the parts table """
+conn = None
 
-if __name__ == '__main__':
-    get_parts()
+try:
+    params = config()
+    conn = psycopg2.connect(**params)
+        # cur = conn.cursor()
+        # cur.execute("SELECT * FROM category2")
+        # rows = cur.fetchall()
+        # print("The number of parts: ", cur.rowcount)
+        # for row in rows:
+        #     print(row)
+        # cur.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+    # finally:
+    #     if conn is not None:
+    #         conn.close()
+
+cur = conn.cursor()
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/v_timestamp')
+def v_timestamp():
+    cur.execute("SELECT * FROM category2")
+    data = cur.fetchall()
+    return render_template('v_timestamp.html', data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# article to display data https://kanchanardj.medium.com/how-to-display-database-content-in-your-flask-website-8a62492ba892
+# querying data from postgresql https://www.postgresqltutorial.com/postgresql-python/query/
 
